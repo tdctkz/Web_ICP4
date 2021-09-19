603a83f9a0f2d38cf671ebb40725ab5d02ce8f73
tdctkz <54277596+tdctkz@users.noreply.github.com>
2021-09-18 21:41:38 -0500

    final

--- sources/GitHubUserFinder/script.js
+++ sources/GitHubUserFinder/script.js
@@ -1,18 +1,30 @@
 function getGithubInfo(user) {
     //1. Create an instance of XMLHttpRequest class and send a GET request using it.
     // The function should finally return the object(it now contains the response!)
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "https://api.github.com/users/" + user, false);
+    xhttp.send();
+    return xhttp;
 }
 
 function showUser(user) {
     //2. set the contents of the h2 and the two div elements in the div '#profile' with the user content
+    $("#image").html("<img height='150' width='150' src='" + user.avatar_url + "'/>");
+    $("#name").text("Name: " + user.name);
+    $("#user_name").text("Username: " + user.login);
+    $("#user_id").text("User Id: " + user.id);
+    $("#user_followers").text("Followers: " + user.followers);
+    $("#user_following").text("Following: " + user.following);
+    $("#user_url").html("<a href='"+user.html_url+"' target='_blank'>GitHub account link</a>");
 }
 
 function noSuchUser(username) {
     //3. set the elements such that a suitable message is displayed
+    alert("User " + username + " not match!!!");
 }
 
 $(document).ready(function () {
-    $(document).on('keypress', '#username', function (e) {
+    $(document).on('keypress',  '#username', function (e) {
         //check if the enter(i.e return) key is pressed
         if (e.which == 13) {
             //get what the user enters
